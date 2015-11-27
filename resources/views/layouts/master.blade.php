79e3b03eebb40d85d3264c5b6104c4dc1b15ed78
Seth Ryder <ryder.seth@gmail.com>
2015-11-26 20:19:24 -0500

    add description

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -6,8 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <meta name="description" content="">
-    <meta name="author" content="">
+    <meta name="description" content="Add the Brazzers logo to any images to completely change the meaning.">
     <link rel="icon" href="../../favicon.ico">
 
     <title>Brazzify.me</title>
